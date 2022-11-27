a02c97275e4a63564d9eed40e27b846d5e014c04
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-28 03:12:40 +0500

    Golang App Arduino

--- app/src/main/java/app/lodemsu/smarthome/MainActivity.java
+++ app/src/main/java/app/lodemsu/smarthome/MainActivity.java
@@ -4,12 +4,17 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
-        
+
     }
 }
