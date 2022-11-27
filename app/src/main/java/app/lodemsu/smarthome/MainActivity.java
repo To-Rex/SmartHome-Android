5ca599c4a0d70e3b18d7cf97a1e50d21d160e535
Dilshodjon <89077434+Yorvoration@users.noreply.github.com>
2022-11-28 03:36:11 +0500

    Golang App Arduino

--- app/src/main/java/app/lodemsu/smarthome/MainActivity.java
+++ app/src/main/java/app/lodemsu/smarthome/MainActivity.java
@@ -8,16 +8,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Objects;
+
 public class MainActivity extends AppCompatActivity {
     private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Objects.requireNonNull(getSupportActionBar()).hide();
         setContentView(R.layout.activity_main);
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Switch switch1 = findViewById(R.id.switch1);
-        //firebase realtime database home in on = value 1
+
+
+
         switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
             if (isChecked) {
                 mDatabase.child("home").child("on").setValue(1);
